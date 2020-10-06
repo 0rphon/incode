58e89497bb53930338f7df435550ee28ff55bff1
0rphon <59403052+0rphon@users.noreply.github.com>
2020-10-06 04:31:48 -0700

    Revert "Revert "updating documentation""
    This reverts commit caa8b3092e50556ce3576e014c4747a3ee824302.

--- src/translate.rs
+++ src/translate.rs
@@ -19,26 +19,26 @@ pub fn parse_bytes(input: &str) -> DynResult<Vec<u8>> {
     Ok(bytes)
 }
 
-pub fn get_words(bytes: &Vec<u8>) -> Vec<[u8;4]> {
+///gets dwords out of a byte array
+pub fn get_dwords(bytes: &Vec<u8>) -> Vec<[u8;4]> {
     let mut words = bytes.chunks_exact(4).map(|b| b.try_into().unwrap())
         .collect::<Vec<[u8;4]>>();
     words.reverse();
     words
 }
 
-///ONLY WORKS FOR X86
+/// converts a u32 into its corresponding bytes
 pub fn get_bytes_u32(val: u32) -> [u8;4] {
     let bytes: [u8;4] = unsafe { transmute(val.to_le()) };
     bytes
 }
 
-///ONLY WORKS FOR X86
-/// will be backwards from the passed bytes obviously
+/// gets the u32 number of a byte array
 pub fn get_u32(bytes: [u8;4]) -> u32 {
     unsafe {transmute(bytes)}
 }
 
-
+/// creates a string out of a byte array
 pub fn format_bytes(bytes: &Vec<u8>) -> String {
     let mut s = String::new();
     s.push('"');
